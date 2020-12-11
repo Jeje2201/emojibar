5f8c00707bcf317cde981c66e0de1505f2906e1b
Jeje2201 <jeremy_leriche@natural-solutions.eu>
2020-12-11 18:11:49 +0100

    show all face emojis and not some pre define

--- src/emoji.js
+++ src/emoji.js
@@ -2,8 +2,8 @@ import emoji from 'emojilib';
 import Fuse from 'fuse.js';
 import { supportedemojis } from './data/supported_emojis';
 
-// Top emojis on Facebook (see https://www.theverge.com/2017/7/17/15984204/facebooks-most-used-emoji-hearts-tears)
-const popularEmojiNames = ['joy', 'heart_eyes', 'kissing_heart', 'laughing', 'grinning', 'heart', 'wink', 'blush', 'sob', 'smile', 'sweat', 'sweat_smile', '+1', 'expressionless', '100', 'cry', 'neutral_face', 'scream', 'ok_hand', 'pensive', 'weary', 'grin', 'smirk'];
+// List all faces by default, stop on the last faces
+const allEmojiFaces = supportedemojis.slice(0, supportedemojis.indexOf('policewoman'));
 
 // List of emojis supported by FB Messenger scraped off https://emojipedia/facebook using src/scripts/get-supported-emoji.py
 const supportedEmojiNames = supportedemojis;
@@ -24,8 +24,8 @@ const emojis = Object.keys(emoji.lib)
 
 module.exports = {
     emojis,
-    popularEmojis: emojis.filter(emojiItem => popularEmojiNames.includes(emojiItem.name))
-        .sort((item1, item2) => popularEmojiNames.indexOf(item1.name) - popularEmojiNames.indexOf(item2.name)),
+    popularEmojis: emojis.filter(emojiItem => allEmojiFaces.includes(emojiItem.name))
+        .sort((item1, item2) => allEmojiFaces.indexOf(item1.name) - allEmojiFaces.indexOf(item2.name)),
     exactSearchEngine: new Fuse(emojis, { ...searchOptions, keys: ['name'] }),
     fuzzySearchEngine: new Fuse(emojis, { ...searchOptions, keys: [{ name: 'name', weight: 0.9 }, { name: 'keywords', weight: 0.1 }] }),
 };
